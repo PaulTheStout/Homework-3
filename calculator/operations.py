ab14f5ad5350d2a3ddc273b283a619d774b38933
PaulTheStout <paulellameh7@gmail.com>
2024-06-12 21:22:42 -0400

    Added 4 functions to operations, add, sub, mult, and div

---
+++ calculator/operations.py
@@ -0,0 +1,16 @@
+from decimal import Decimal
+
+
+def add(a: Decimal, b: Decimal) -> Decimal: #(Type hints are to ensure that a and b are decimal values and -> Decimal hints that a decimal is returned)
+    return a + b
+
+def sub(a: Decimal, b: Decimal) -> Decimal:
+    return a - b
+
+def mult(a: Decimal, b: Decimal) -> Decimal:
+    return a * b
+
+def div(a: Decimal, b: Decimal) -> Decimal:
+    if b == 0:
+        raise valueError("Error: Can't divide number by 0")
+    return a / b
