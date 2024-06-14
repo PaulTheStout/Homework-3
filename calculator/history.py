4efcb4646d3f3f291247a5f3524381e50f22ab5d
PaulTheStout <paulellameh7@gmail.com>
2024-06-13 20:47:55 -0400

    Added 4 methods, one to add calculations, one to get the last calculation, and the other to get all history

---
+++ calculator/history.py
@@ -0,0 +1,39 @@
+"""
+This module manages the history of various calculations
+"""
+
+from typing import List
+from calculator_oper import CalculatorOper
+
+class History:
+    """
+    This is a class with several methods to track history of calculations
+    """
+    _history: List[CalculatorOper] = []
+
+    def add_calculation(self, calculation: CalculatorOper) -> None:
+        """
+        Adds a new calculation to the history.
+        """
+        self._history.append(calculation)
+
+    def get_last(self) -> CalculatorOper:
+        """
+        Retrieves the last calculation from history.
+        If none, return empty
+        """
+        if not self._history:
+            return None
+        return self._history[-1]
+
+    def get_all(self) -> List[CalculatorOper]:
+        """
+        Gets all calculations from the history
+        """
+        return self._history
+
+    def __repr__(self):
+        """
+        Returns the string representation of the class.
+        """
+        return f"History({self._history})"
