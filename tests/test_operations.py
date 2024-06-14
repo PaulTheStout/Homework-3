6a0e8c0d9708e0c7da123a7f00f7b3cdd56b6fa8
PaulTheStout <paulellameh7@gmail.com>
2024-06-13 23:04:25 -0400

    Added several test to see if operations work correctly

--- tests/test_operations.py
+++ tests/test_operations.py
@@ -1,22 +1,52 @@
-"""This is a test file that has two functions, the first is to test different operations and ensure
-   that the they work, otherwise, a error message is thrown saying that the operation has failed. 
-   The 2nd function is to a test that has a number divided by 0, and throws a error saying you can't
-   divide by 0."""
+"""
+This is a module to test the operators from operators.py
+"""
 
-import pytest
 from decimal import Decimal
-from calculator.calcOper import calcOper
+import pytest
 from calculator.operations import add, sub, mult, div
 
+def test_addition():
+    """
+    Test if addition works properly
+    """
+    assert add(3, 4) == Decimal(7)
 
-def test_Op(a: Decimal, b: Decimal, operation, expected):
-    calculation = calcOper.create(a, b, operation)
-    assert calcOper.perform() == expected, f"{operation.__name__} operation has failed"
-    '''Tests that operations work, otherwise the operation has failed.'''
+def test_subtraction():
+    """
+    Test if subtraction works properly
+    """
+    assert sub(5, 2) == Decimal(3)
+
+def test_multiplication():
+    """
+    Test if multiplication works properly
+    """
+    assert mult(2, 3) == Decimal(6)
+
+def test_division():
+    """
+    Test if division works properly
+    """
+    assert div(10, 2) == Decimal(5)
+
+def test_division_by_zero():
+    """
+    Test if divide by zero raises value error
+    """
+    with pytest.raises(ValueError):
+        div(5, 0)
+
+def test_float_addition():
+    """
+    Test that floating addition works
+    """
+    result = round(add(0.1, 0.2), 1)
+    assert Decimal(str(result)) == Decimal("0.3")
 
 
-def test_Div_Zero():
-    with pytest.raises(ValueError, match = "Error: Can't divide number by 0"):
-        calculation = calcOper(Decimal('7'), Decimal('0'), div)
-        calculation.perform()
-        '''Is a test to throw an error if a number is divided by zero, if zero then throw error'''
+def test_negative_subtraction():
+    """
+    Test if negative subtraction works properly
+    """
+    assert sub(-5, -2) == Decimal(-3)
